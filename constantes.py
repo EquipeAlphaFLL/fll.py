1603d93eec6ddf9f78d92d660f3903e448d10a28
EquipeAlphaFLL <alpha01fll@gmail.com>
2025-11-01 11:49:57 -0300

    Create constantes.py

---
+++ constantes.py
@@ -0,0 +1,17 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from drive_train import mt_crema, mt_cremaII
+
+hub = PrimeHub()
+
+
+def subirI():
+    mt_crema.run_angle(1000, 180)
+    #antihorario
+
+def descerI():
+    mt_crema.run_angle(1000,-180)
+    #horario
